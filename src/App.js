d0312026cd62ab3d8a4b7d30163c7607d402527b
altmit <chlwodnjs34@gmail.com>
2023-03-04 17:14:32 +0900

    [feat] 감정 일기를 만들기 위한 기초 설정 라우팅, 폰트, 공용 레이아웃, 감정 이미지, 공용 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
+        <h2>App</h2>
+
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼")}
+          type={"negative"}
+        />
+        <MyButton text={"버튼"} onClick={() => alert("버튼")} />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary/:id" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
-
